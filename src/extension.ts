d07c2693e973d6fe8b8b5cdff1f8ae9cc9f6fa89
Alfish <101953341+AlfishSoftware@users.noreply.github.com>
2022-03-24 05:13:52 -0300

    Proper escaped string and tokenized quotes

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,31 @@
 import * as vscode from 'vscode';
+function escapeReplacement(s: string) { return s.replace(/\$/g, '$$$$'); }
 
 class GDAssetProvider implements vscode.DocumentSymbolProvider {
   static docs: vscode.DocumentSelector = 'gdasset';
+  
+  public static unescapeString(partInsideQuotes: string) {
+    let s = '';
+    for (const m of partInsideQuotes.matchAll(/\\(["ntr\\bf]|u[0-9A-Fa-f]{4})|\\$|\\?([^])/g)) {
+      switch (m[1]) {
+        case '\\': case '"': s += m[1]; continue;
+        case 'n': s += '\n'; continue;
+        case 't': s += '\t'; continue;
+        case 'r': s += '\r'; continue;
+        case 'b': s += '\b'; continue;
+        case 'f': s += '\f'; continue;
+        case undefined: case null: case '': s += m[2] ?? ''; continue;
+        default: s += String.fromCharCode(parseInt(m[1].substring(1), 16)); continue; // uXXXX
+      }
+    }
+    return s;
+  }
+  
   public async provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken
   ): Promise<vscode.DocumentSymbol[]> {
     let rootNode: string | undefined;
     function nodePath(n: string) {
-      return rootNode ? n.replace(/^\.(?=\/|$)/, rootNode) : n;
+      return rootNode ? n.replace(/^\.(?=\/|$)/, escapeReplacement(rootNode)) : n;
     }
     const symbols = [];
     for (let i = 0; i < document.lineCount; i++) {
@@ -17,7 +36,7 @@ class GDAssetProvider implements vscode.DocumentSymbolProvider {
         const [, header, tag, rest] = match;
         const attributes: { [field: string]: string | undefined } = {};
         for (const assignment of rest.matchAll(/\b([\w-]+)\b\s*=\s*(?:(\d+)|"([^"]*)")/g))
-          attributes[assignment[1]] = assignment[2] ?? assignment[3];
+          attributes[assignment[1]] = assignment[2] ?? GDAssetProvider.unescapeString(assignment[3]);
         let name: string, detail, kind;
         switch (tag) {
           case 'gd_scene':
