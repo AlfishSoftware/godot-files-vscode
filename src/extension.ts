1cfe111475d73c0d61a2880a5593daee3cdebfa2
Alfish <101953341+AlfishSoftware@users.noreply.github.com>
2022-03-24 03:40:21 -0300

    Outline items for ini section headers

---
+++ src/extension.ts
@@ -0,0 +1,72 @@
+import * as vscode from 'vscode';
+
+class GDAssetProvider implements vscode.DocumentSymbolProvider {
+  static docs: vscode.DocumentSelector = 'gdasset';
+  public async provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken
+  ): Promise<vscode.DocumentSymbol[]> {
+    let rootNode: string | undefined;
+    function nodePath(n: string) {
+      return rootNode ? n.replace(/^\.(?=\/|$)/, rootNode) : n;
+    }
+    const symbols = [];
+    for (let i = 0; i < document.lineCount; i++) {
+      const line = document.lineAt(i);
+      const match =
+        line.text.match(/^\s*(\[\s*([^\[\]\s]+)\s*(.*?)\s*\])\s*([;#].*)?$/);
+      if (match) {
+        const [, header, tag, rest] = match;
+        const attributes: { [field: string]: string | undefined } = {};
+        for (const assignment of rest.matchAll(/\b([\w-]+)\b\s*=\s*(?:(\d+)|"([^"]*)")/g))
+          attributes[assignment[1]] = assignment[2] ?? assignment[3];
+        let name: string, detail, kind;
+        switch (tag) {
+          case 'gd_scene':
+            name = document.uri.path.replace(/^\/(?:.*\/)*(.*?)(?:\.[^.]*)?$/, '$1');
+            detail = 'PackedScene';
+            kind = vscode.SymbolKind.File;
+            break;
+          case 'gd_resource':
+            name = document.uri.path.replace(/^\/(.*\/)*/, '');
+            detail = attributes.type;
+            kind = vscode.SymbolKind.File;
+            break;
+          case 'ext_resource':
+            name = attributes.path ?? tag;
+            detail = attributes.type;
+            kind = vscode.SymbolKind.Variable;
+            break;
+          case 'sub_resource':
+            name = '::' + attributes.id;
+            detail = attributes.type;
+            kind = vscode.SymbolKind.Object;
+            break;
+          case 'node':
+            if (attributes.parent == undefined)
+              name = (rootNode = attributes.name) ?? tag;
+            else name = nodePath(attributes.parent) + '/' + attributes.name;
+            detail = attributes.type;
+            kind = vscode.SymbolKind.Object;
+            break;
+          case 'connection':
+            if (attributes.from && attributes.to && attributes.method)
+              name = `${nodePath(attributes.from)}→${nodePath(attributes.to)}::${attributes.method}`;
+            else name = tag;
+            detail = attributes.signal;
+            kind = vscode.SymbolKind.Event;
+            break;
+          default:
+            name = tag;
+            detail = rest;
+            kind = vscode.SymbolKind.Object;
+            break;
+        }
+        symbols.push(new vscode.DocumentSymbol(name, detail ?? '', kind, line.range, line.range));
+      }
+    }
+    return symbols;
+  }
+}
+
+export function activate(ctx: vscode.ExtensionContext) {
+  ctx.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(GDAssetProvider.docs, new GDAssetProvider()));
+}
