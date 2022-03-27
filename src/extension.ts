022cbeb4c802e300fc1b58e461fc2478413dcbf8
Alfish <101953341+AlfishSoftware@users.noreply.github.com>
2022-03-26 23:02:38 -0300

    Tree outline items for ini properties

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,70 @@
 import * as vscode from 'vscode';
 function escapeReplacement(s: string) { return s.replace(/\$/g, '$$$$'); }
 
+class GDAsset {
+  rootNode: string | undefined = undefined;
+  nodePath(n: string) {
+    return this.rootNode ? n.replace(/^\.(?=\/|$)/, escapeReplacement(this.rootNode)) : n;
+  }
+}
+
+function makeSectionSymbol(
+  document: vscode.TextDocument,
+  match: RegExpMatchArray,
+  line: vscode.TextLine,
+  gdasset: GDAsset,
+) {
+  const [, header, tag, rest] = match;
+  const attributes: { [field: string]: string | undefined } = {};
+  for (const assignment of rest.matchAll(/\b([\w-]+)\b\s*=\s*(?:(\d+)|"([^"]*)")/g))
+    attributes[assignment[1]] = assignment[2] ?? GDAssetProvider.unescapeString(assignment[3]);
+  let name: string, detail, kind;
+  switch (tag) {
+    case 'gd_scene':
+      name = document.uri.path.replace(/^\/(?:.*\/)*(.*?)(?:\.[^.]*)?$/, '$1');
+      detail = 'PackedScene';
+      kind = vscode.SymbolKind.File;
+      break;
+    case 'gd_resource':
+      name = document.uri.path.replace(/^\/(.*\/)*/, '');
+      detail = attributes.type;
+      kind = vscode.SymbolKind.File;
+      break;
+    case 'ext_resource':
+      name = attributes.path ?? tag;
+      detail = attributes.type;
+      kind = vscode.SymbolKind.Variable;
+      break;
+    case 'sub_resource':
+      name = '::' + attributes.id;
+      detail = attributes.type;
+      kind = vscode.SymbolKind.Object;
+      break;
+    case 'node':
+      if (attributes.parent == undefined)
+        name = (gdasset.rootNode = attributes.name) ?? tag;
+      else name = gdasset.nodePath(attributes.parent) + '/' + attributes.name;
+      detail = attributes.type;
+      kind = vscode.SymbolKind.Object;
+      break;
+    case 'connection':
+      if (attributes.from && attributes.to && attributes.method)
+        name = `${gdasset.nodePath(attributes.from)}→${gdasset.nodePath(attributes.to)}::${attributes.method}`;
+      else name = tag;
+      detail = attributes.signal;
+      kind = vscode.SymbolKind.Event;
+      break;
+    default:
+      name = tag;
+      detail = rest;
+      kind = vscode.SymbolKind.Object;
+      break;
+  }
+  return new vscode.DocumentSymbol(name, detail ?? '', kind, line.range, line.range);
+}
+
 class GDAssetProvider implements vscode.DocumentSymbolProvider {
-  static docs: vscode.DocumentSelector = 'gdasset';
+  static docs: vscode.DocumentSelector = ['gdasset', 'config-definition'];
   
   public static unescapeString(partInsideQuotes: string) {
     let s = '';
@@ -23,65 +85,54 @@ class GDAssetProvider implements vscode.DocumentSymbolProvider {
   
   public async provideDocumentSymbols(document: vscode.TextDocument, token: vscode.CancellationToken
   ): Promise<vscode.DocumentSymbol[]> {
-    let rootNode: string | undefined;
-    function nodePath(n: string) {
-      return rootNode ? n.replace(/^\.(?=\/|$)/, escapeReplacement(rootNode)) : n;
-    }
+    const gdasset = document.languageId == 'gdasset' ? new GDAsset() : null;
+    let previousLine: vscode.TextLine | undefined;
+    let currentSection: vscode.DocumentSymbol | undefined;
+    let currentProperty: vscode.DocumentSymbol | null = null;
     const symbols = [];
     for (let i = 0; i < document.lineCount; i++) {
       const line = document.lineAt(i);
-      const match =
-        line.text.match(/^\s*(\[\s*([^\[\]\s]+)\s*(.*?)\s*\])\s*([;#].*)?$/);
+      let match = line.text.match(
+        /^(\[\s*([\p{L}\w-]+(?:\s+[\p{L}\w-]+|\s+"[^"\\]*")*(?=\s*\])|[^\[\]\s]+)\s*(.*?)\s*\])\s*([;#].*)?$/u
+      );
       if (match) {
-        const [, header, tag, rest] = match;
-        const attributes: { [field: string]: string | undefined } = {};
-        for (const assignment of rest.matchAll(/\b([\w-]+)\b\s*=\s*(?:(\d+)|"([^"]*)")/g))
-          attributes[assignment[1]] = assignment[2] ?? GDAssetProvider.unescapeString(assignment[3]);
-        let name: string, detail, kind;
-        switch (tag) {
-          case 'gd_scene':
-            name = document.uri.path.replace(/^\/(?:.*\/)*(.*?)(?:\.[^.]*)?$/, '$1');
-            detail = 'PackedScene';
-            kind = vscode.SymbolKind.File;
-            break;
-          case 'gd_resource':
-            name = document.uri.path.replace(/^\/(.*\/)*/, '');
-            detail = attributes.type;
-            kind = vscode.SymbolKind.File;
-            break;
-          case 'ext_resource':
-            name = attributes.path ?? tag;
-            detail = attributes.type;
-            kind = vscode.SymbolKind.Variable;
-            break;
-          case 'sub_resource':
-            name = '::' + attributes.id;
-            detail = attributes.type;
-            kind = vscode.SymbolKind.Object;
-            break;
-          case 'node':
-            if (attributes.parent == undefined)
-              name = (rootNode = attributes.name) ?? tag;
-            else name = nodePath(attributes.parent) + '/' + attributes.name;
-            detail = attributes.type;
-            kind = vscode.SymbolKind.Object;
-            break;
-          case 'connection':
-            if (attributes.from && attributes.to && attributes.method)
-              name = `${nodePath(attributes.from)}→${nodePath(attributes.to)}::${attributes.method}`;
-            else name = tag;
-            detail = attributes.signal;
-            kind = vscode.SymbolKind.Event;
-            break;
-          default:
-            name = tag;
-            detail = rest;
-            kind = vscode.SymbolKind.Object;
-            break;
+        // Section Header
+        if (currentSection && previousLine)
+          currentSection.range = new vscode.Range(currentSection.range.start, previousLine.range.end);
+        if (gdasset)
+          currentSection = makeSectionSymbol(document, match, line, gdasset);
+        else {
+          const [, header, tag, rest] = match;
+          currentSection = new vscode.DocumentSymbol(tag, rest, vscode.SymbolKind.Object, line.range, line.range);
         }
-        symbols.push(new vscode.DocumentSymbol(name, detail ?? '', kind, line.range, line.range));
+        symbols.push(currentSection);
+        currentProperty = null;
+      } else if (match = line.text.match(
+        /^(((?:[\p{L}\w-]+[./])*[\p{L}\w-]+)(?:\s*\[([\w\\/.:!@$%+-]+)\])?)\s*=/u
+      )) {
+        // Property Assignment
+        const [, prop, key, index] = match;
+        let s = currentSection?.children ?? symbols;
+        if (index) {
+          const p = `${key}[]`;
+          let parentProp = s.find(value => value.name == p);
+          if (!parentProp)
+            s.push(parentProp = new vscode.DocumentSymbol(p, '', vscode.SymbolKind.Array, line.range, line.range));
+          parentProp.range = new vscode.Range(parentProp.range.start, line.range.end);
+          s = parentProp.children;
+        }
+        if (currentSection)
+          currentSection.range = new vscode.Range(currentSection.range.start, line.range.end);
+        currentProperty = new vscode.DocumentSymbol(prop, '', vscode.SymbolKind.Property, line.range, line.range);
+        s.push(currentProperty);
+      } else if (currentProperty && !/^\s*(?:[;#].*)?$/.test(line.text)) {
+        // Still in value of previous property
+        currentProperty.range = new vscode.Range(currentProperty.range.start, line.range.end);
       }
+      previousLine = line;
     }
+    if (currentSection && previousLine)
+      currentSection.range = new vscode.Range(currentSection.range.start, previousLine.range.end);
     return symbols;
   }
 }
