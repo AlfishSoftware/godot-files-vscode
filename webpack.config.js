f0e6d762fc78f578dea2cedbcf40f2d8b0acedb4
Alfish <101953341+AlfishSoftware@users.noreply.github.com>
2024-07-25 07:08:23 -0300

    Make webpack produce more debuggable output code with proper source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-/** @type import('webpack').Configuration */ module.exports = {
-  mode: 'none', // leave source code as close as possible to the original (when packaging we set this to 'production')
+let repo = '../';
+/** @type import('webpack').Configuration */ const config = {
+  mode: 'development', // leave source code close to the original (when packaging we set this to 'production')
   target: 'webworker', // extensions run in a webworker context
   entry: {
     extension: './src/ExtensionEntry.ts', // source of the web extension main file
@@ -8,7 +9,19 @@
     filename: 'extension.js',
     path: __dirname + '/dist@web/',
     libraryTarget: 'commonjs',
-    devtoolModuleFilenameTemplate: '../[resource-path]',
+    devtoolModuleFilenameTemplate: info => {
+      const resourcePath = `${info.resourcePath}`.replace(/^\.\//, '');
+      if (/^ignored\|/i.test(resourcePath)) return `webpack://${info.namespace}/gen/ignored-${info.hash}.js`;
+      const m = /^external (\w+) "(.*?)"$/i.exec(resourcePath);
+      if (m) return `webpack://${info.namespace}/gen/external-${m[1]}-${m[2]}.js`;
+      const root = /^webpack\//i.test(resourcePath) ? `webpack://${info.namespace}/` : repo;
+      return root + (/\.[jt]s$/i.test(resourcePath) ? resourcePath : resourcePath + '.js');
+    },
+  },
+  optimization: {
+    minimize: false,
+    moduleIds: 'named',
+    chunkIds: 'named',
   },
   resolve: {
     mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
@@ -51,4 +64,13 @@
     hints: false,
   },
   devtool: 'nosources-source-map', // create a source map that points to the original source file
+};
+module.exports = (env, argv) => {
+  if (argv.mode == 'production') {
+    config.devtool = 'source-map'; // include full source files in the source maps
+    // make source map links that point to files in the online repo
+    const pkg = require('./package.json');
+    repo = `${pkg.repository.url}/raw/v${pkg.version}/`;
+  }
+  return config;
 };
